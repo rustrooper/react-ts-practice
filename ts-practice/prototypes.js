ee43fa7c2521dbe822841b669851069299d87ece
Daniel <dsimonovd@gmail.com>
2026-01-29 19:39:10 +0300

    modified

--- ts-practice/prototypes.js
+++ ts-practice/prototypes.js
@@ -11,12 +11,25 @@ let rabbit = {
 	__proto__: animal,
 }
 
+// rabbit.__proto__ = animal
+console.log(rabbit.eats)
+rabbit.walk()
+// console.log(longEar.jumps)
+
 let longEar = {
 	earLength: 10,
 	__proto__: rabbit,
 }
 
-// rabbit.__proto__ = animal
-console.log(rabbit.eats)
-rabbit.walk()
-console.log(longEar.jumps)
+let descriptor = Object.getOwnPropertyDescriptor(longEar, 'earLength')
+console.log(descriptor)
+Object.defineProperty(longEar, 'earLength', {writable: true, configurable: false})
+descriptor = Object.getOwnPropertyDescriptor(longEar, 'earLength')
+console.log(descriptor)
+
+longEar.earLength = 11
+Object.defineProperty(longEar, 'earLength', {writable: false})
+descriptor = Object.getOwnPropertyDescriptor(longEar, 'earLength')
+console.log(descriptor)
+
+Object.defineProperty(longEar, 'earLength', {writable: true})
